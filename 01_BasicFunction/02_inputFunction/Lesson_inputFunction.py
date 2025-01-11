e2dad7b33ac2a510751a5dbe4496376e8cc11079
Jessie Jones <Jessie.Jones14@protonmail.com>
2025-01-11 10:59:29 -0500

    Input done

---
+++ 01_BasicFunction/02_inputFunction/Lesson_inputFunction.py
@@ -0,0 +1,19 @@
+# input function
+# // Remove the # and run the code, 
+# debug the function as needed.
+
+#input("What is your name?)
+
+# Embed the input function inside the print function like we did with the type function
+# // Remove the # and run the code,  
+# debug the function as needed
+
+#print("Hello" + input("What is your name?") + "!")
+
+# putting a function inside another function is called a nested function
+
+#what is the difference between this:
+#print("Hello" + input("What is your name?") + "!")
+
+#and this...
+#print("Hello" + input("What is your name?" + "!"))

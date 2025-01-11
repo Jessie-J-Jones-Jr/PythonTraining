92447d464fb10d5c1942a585dac81b8fc2488970
Jessie Jones <Jessie.Jones14@protonmail.com>
2025-01-11 15:17:26 -0500

    Correct DT quiz easy

--- 01_BasicFunction/04_primitiveDataTypes/Z_Quiz_DataType_Errors.py
+++ 01_BasicFunction/04_primitiveDataTypes/Z_Quiz_DataType_Errors.py
@@ -3,121 +3,123 @@
 # Recomment Out the function. 
 # Fix the issues below each problematic line. 
 
-# 1.
- len(12345)
+# 1. TypeError: object of type 'int' has no len()
+#print(len(12345))
 
 # 2.
-# print("The number is: " + 42)
+#print("The number is: " + 42)
 
 # 3.
-# for char in 1234:
-#     print(char)
+#for char in 1234:
+#print(char)
 
 # 4.
-# total = 10 + "20"
+#total = 10 + "20"
+#print(total)
 
 # 5.
-# import math
-# print(math.sqrt("16"))
+#import math
+#print(math.sqrt("16"))
 
 # 6.
-# num = 12345
-# print(num[0])
+#num = 12345
+#print(num[0])
 
 # 7.
-# result = "Hi" * "5"
+#result = "Hi" * "5"
+#print(result)
 
 # 8.
-# for i in range(5.0):
-#     print(i)
+#for i in range(5.0):
+#print(i)
 
 # 9.
-# length = len
-# print(length([1, 2, 3]))
+#length = len
+#print(length([1, 2, 3]))
 
 # 10.
-# print("The value of x is: {} and y is: {}".format(10))
+#print("The value of x is: {} and y is: {}".format(10))
 
 # 11.
-# data = {"key": "value"}
-# print(data[0])
+#data = {"key": "value"}
+#print(data[0])
 
 # 12.
-# x = "10"
-# y = x + 5
-# print(y)
+#x = "10"
+#y = x + 5
+#print(y)
 
 # 13.
-# lst = [1, 2, 3]
-# print(lst["0"])
+#lst = [1, 2, 3]
+#print(lst["0"])
 
 # 14.
-# print(3 / "3")
+#print(3 / "3")
 
 # 15.
-# print(int("abc"))
+#print(int("abc"))
 
 # 16.
-# def func():
-#     return
-# print(func() + 10)
+#def func():
+# return
+#print(func() + 10)
 
 # 17.
-# float_value = "123.45"
-# print(float_value + 10)
+#float_value = "123.45"
+#print(float_value + 10)
 
 # 18.
-# result = "10" - 5
-# print(result)
+#result = "10" - 5
+#print(result)
 
 # 19.
-# x = 20
-# y = "30"
-# print(x + y)
+#x = 20
+#y = "30"
+#print(x + y)
 
 # 20.
-# lst = [1, 2, 3]
-# print(lst[5])
+#lst = [1, 2, 3]
+#print(lst[5])
 
 # 21.
-# import math
-# print(math.pow("2", 3))
+#import math
+#print(math.pow("2", 3))
 
 # 22.
-# name = "John"
-# print(name + 25)
+#name = "John"
+#print(name + 25)
 
 # 23.
-# name = "Alice"
-# age = "25"
-# print("Name: " + name + ", Age: " + age + ", Next year: " + age + 1)
+#name = "Alice"
+#age = "25"
+#print("Name: " + name + ", Age: " + age + ", Next year: " + age + 1)
 
 # 24.
-# def add_numbers(a, b):
-#     return a + b
-# print(add_numbers("Hello", 5))
+#def add_numbers(a, b):
+#  return a + b
+#print(add_numbers("Hello", 5))
 
 # 25.
-# number = 10
-# result = number + "abc"
-# print(result)
+#number = 10
+#result = number + "abc"
+#print(result)
 
 # 26.
-# a = [1, 2, 3]
-# print(a["1"])
+#a = [1, 2, 3]
+#print(a["1"])
 
 # 27.
-# age = "twenty"
-# print(int(age))
+#age = "twenty"
+#print(int(age))
 
 # 28.
-# data = ["a", "b", "c"]
-# print(data + 5)
+#data = ["a", "b", "c"]
+#print(data + 5)
 
 # 29.
-# value = "123"
-# print(value * "10")
+#value = "123"
+#print(value * "10")
 
 # 30.
-# x = None
-# print(x + 10)
+#x = None
+#print(x + 10)
---
+++ 01_BasicFunction/04_primitiveDataTypes/Z_Quiz_DataType_Errors_Tough.py
@@ -0,0 +1,65 @@
+# Fix the issues below each problematic line.
+
+# 1.
+# data = [1, 2, 3]
+# print(data + "4")
+
+# 2.
+# result = "5" * 3.0
+# print(result)
+
+# 3.
+# a = "100"
+# b = 200
+# print(a * b)
+
+# 4.
+# lst = [1, 2, 3]
+# print(lst["1"])
+
+# 5.
+# def add(a, b):
+#     return a + b
+# print(add("Hello", [1, 2]))
+
+# 6.
+# name = "Alice"
+# print(name[10])
+
+# 7.
+# value = [1, 2, 3]
+# print(value + None)
+
+# 8.
+# x = 10
+# print(x + None)
+
+# 9.
+# print("Value is: " + None)
+
+# 10.
+# dict_obj = {1: "a", 2: "b"}
+# print(dict_obj[None])
+
+# 11.
+# a = 5
+# b = "10"
+# print(f"Sum: {a} + {b} = {a + b}")
+
+# 12.
+# num = 10
+# print(len(num))
+
+# 13.
+# result = 5 ** "2"
+# print(result)
+
+# 14.
+# def test_func(a, b):
+#     return a + b
+# print(test_func("text", 10.5))
+
+# 15.
+# x = {1, 2, 3}
+# y = [4, 5, 6]
+# print(x + y)

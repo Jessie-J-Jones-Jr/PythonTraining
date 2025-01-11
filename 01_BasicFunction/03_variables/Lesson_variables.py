95923c058d7c54400a719afdb1631653f0da44d3
Jessie Jones <Jessie.Jones14@protonmail.com>
2025-01-11 11:19:15 -0500

    Variables done

--- 01_BasicFunction/03_variables/Lesson_variables.py
+++ 01_BasicFunction/03_variables/Lesson_variables.py
@@ -1,49 +1,131 @@
 #functions within print function print(type(value))
-print(type("Hello Word!"))
+#//Remove the # and run the code
 
-#different types of variables within Python
+#print(type("Hello Word!"))
 
-# Integer
-age = 30
-print(type(age))  # Output: <class 'int'>
+
+
+############Different types of variables within Python###########
+
+
+#Integer
+#//Remove the # and run the code
+#age = 30
+#print(type(age))  # Output: <class 'int'>
+
 # Integer: Whole numbers, positive or negative, without a decimal point.
 
+
+
+
 # Float
-height = 5.9
-print(type(height))  # Output: <class 'float'>
+#//Remove the # and run the code
+
+#height = 5.9
+#print(type(height))  # Output: <class 'float'>
+
 # Float: Numbers that have a decimal point or are in scientific notation.
 
+
+
+
 # String
-name = "Alice"
-print(type(name))  # Output: <class 'str'>
+#//Remove the # and run the code
+
+#name = "Alice"
+#print(type(name))  # Output: <class 'str'>
+
 # String: A sequence of characters enclosed in quotes (single or double).
 
+
+
+
 # Boolean
-is_student = True
-print(type(is_student))  # Output: <class 'bool'>
+#//Remove the # and run the code
+
+#is_student = True
+#print(type(is_student))  # Output: <class 'bool'>
+
 # Boolean: A type that can hold one of two values: True or False.
 
 
+
+
+
 # List
-fruits = ["apple", "banana", "cherry"]
-print(type(fruits))  # Output: <class 'list'>
+#//Remove the # and run the code
+
+#fruits = ["apple", "banana", "cherry"]
+#print(type(fruits))  # Output: <class 'list'>
+
 # List: An ordered, mutable collection of items, which can be of different data types.
 
+
+
+
 # Tuple
-coordinates = (10, 20)
-print(type(coordinates))  # Output: <class 'tuple'>
+#//Remove the # and run the code
+
+#coordinates = (10, 20)
+#print(type(coordinates))  # Output: <class 'tuple'>
+
 # Tuple: An ordered, immutable collection of items, which can be of different data types.
 
+
+
+
 # Dictionary
-person = {"name": "Alice", "age": 25}
-print(type(person))  # Output: <class 'dict'>
+#//Remove the # and run the code
+
+#person = {"name": "Alice", "age": 25}
+#print(type(person))  # Output: <class 'dict'>
+
 # Dictionary: An unordered collection of key-value pairs, where each key is unique.
 
+
+
+
 # Set
-unique_numbers = {1, 2, 3}
-print(type(unique_numbers))  # Output: <class 'set'>
+#//Remove the # and run the code
+
+#unique_numbers = {1, 2, 3}
+#print(type(unique_numbers))  # Output: <class 'set'>
+
 # Set: An unordered collection of unique items (no duplicates allowed).
 
+#Use the input function to find the length of the input, try to debug this function.
+
+#name = input("What is your name?")
+#length = len(name)
+
+#print("Hello " + name + "! Your name has " + length + " characters!")
+
+
+
+#Comment out the function above and check with the answer below
+
+
+
+
+
+
+#Answer
+
+
+#name = input("What is your name?")
+#length = str(len(name))
+
+#print("Hello " + name + "! Your name has " + length + " characters!")
+
+
+
+#Bonus Question:
+# How could you do this nested within the print function? Comment out the function above and modify 
+# the function below
+
+
+#name = input("What is your name?")
+#print("Hello " + name + "! Your name has " + name + " characters!")
 
 
 
@@ -55,6 +137,9 @@ print(type(unique_numbers))  # Output: <class 'set'>
 
 
 
+#Answer:
+#name = input("What is your name?")
+#print("Hello " + name + "! Your name has " + str(name) + " characters!")
 
 
 

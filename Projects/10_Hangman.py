7d7e8b2cc406d437976918b6259a7cd9dc136c1c
Jessie Jones <Jessie.Jones14@Protonmail.com>
2025-01-13 19:56:16 -0500

    working on hangman game still working

--- Projects/10_Hangman.py
+++ Projects/10_Hangman.py
@@ -8,8 +8,8 @@ word_vault = []
 for i in range(1):
     new_word = word_create.word(word_min_length=word_length, word_max_length=word_length)
     word_list = list(new_word)
-    print(f"==> word_list {word_list}")
-    print("Word "+str(i+1)+" : ", new_word)
+    # print(f"==> word_list {word_list}")
+    # print("Word "+str(i+1)+" : ", new_word)
 
 for i in range(word_length):
     display_dashes += " _ "
@@ -19,21 +19,29 @@ for i in range(word_length):
 
 print(f"==>> display_dashes: {display_dashes}")
 
-print(f"==>> word_vault: {word_vault}")
+# print(f"==>> word_vault: {word_vault}")
 
 guessed_letter = input("Guess your first letter: ")
 
 for i in range(word_length):
     if guessed_letter == word_list[i]:
-        print("RIGHT")
+        # print("RIGHT")
         word_vault[i] = guessed_letter
-    else:
-        print("WRONG")
-print(word_vault)
+        
+    # else:
+        # print("WRONG")
+# print(f"==>> word_vault: {word_vault}")
 
 display_dashes =""
 for i in range(word_length):
     display_dashes += " " + word_vault[i] +" "
-print(display_dashes)
+    
+print(f"==>> display_dashes: {display_dashes}")
+
+#break up into functions
+# add a while loop and game status variables if the game is still in progress keep allowing for guess
+#with a break out for if the wrong guess counter is exhausted game over
+# neutral check function for if the users guesses the same letter that was already guessed.
+# the finally get graphics
 
 #https://www.askpython.com/python-modules/wonderwords-module

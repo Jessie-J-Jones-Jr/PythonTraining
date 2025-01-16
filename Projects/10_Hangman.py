d57edee856dcd035b4a920af948384d7dd0d8909
Jessie Jones <Jessie.Jones14@Protonmail.com>
2025-01-15 23:18:19 -0500

    finishing the python game

--- Projects/10_Hangman.py
+++ Projects/10_Hangman.py
@@ -4,12 +4,14 @@ word_list = []
 word_length = int(input("How many letters in the word you want to guess?  "))
 display_dashes = ""
 word_vault = []
+game_over = False
+
 
 for i in range(1):
     new_word = word_create.word(word_min_length=word_length, word_max_length=word_length)
     word_list = list(new_word)
     # print(f"==> word_list {word_list}")
-    # print("Word "+str(i+1)+" : ", new_word)
+    print("Word "+str(i+1)+" : ", new_word)
 
 for i in range(word_length):
     display_dashes += " _ "
@@ -17,26 +19,35 @@ for i in range(word_length):
 for i in range(word_length):
     word_vault.append(" _ ")
 
+# print(f"==>> word_vault: {word_vault}")
 print(f"==>> display_dashes: {display_dashes}")
-
-# print(f"==>> word_vault: {word_vault}")
-
-guessed_letter = input("Guess your first letter: ")
-
-for i in range(word_length):
-    if guessed_letter == word_list[i]:
-        # print("RIGHT")
-        word_vault[i] = guessed_letter
-        
-    # else:
-        # print("WRONG")
-# print(f"==>> word_vault: {word_vault}")
-
-display_dashes =""
-for i in range(word_length):
-    display_dashes += " " + word_vault[i] +" "
+while not game_over:
     
-print(f"==>> display_dashes: {display_dashes}")
+    guessed_letter = input("Guess your first letter: ")
+    display_dashes =""
+
+
+    for i in range(word_length):
+        if guessed_letter == word_list[i]:
+            if guessed_letter == word_vault [i]:
+                print(f"You already guessed: {guessed_letter.capitalize()}. Please try again.")
+            else:
+                print("RIGHT")
+                word_vault[i] = " " + guessed_letter + " "
+                #display_dashes += " " + word_vault[i] +" "
+        if guessed_letter not in word_vault:
+            print("Sorry Wrong Letter:")
+
+        if game_over:
+            print("GAME OVER")
+    # print(f"==>> word_vault: {word_vault}")
+    
+    display_dashes = ''.join(word_vault)
+    print(f"==>> display_dashes: {display_dashes}")
+    print(f"==>> word_vault[i]: {word_vault}")
+   
+        
+    #print(f"==>> display_dashes: {display_dashes}")
 
 #break up into functions
 # add a while loop and game status variables if the game is still in progress keep allowing for guess

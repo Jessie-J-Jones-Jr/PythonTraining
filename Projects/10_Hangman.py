7db1abbb05a377952ff0d2c0d0c5d5c8d5a1e35c
Jessie Jones <Jessie.Jones14@Protonmail.com>
2025-01-18 16:55:28 -0500

    fixed issues with functions and the game now can now be replayed

--- Projects/10_Hangman.py
+++ Projects/10_Hangman.py
@@ -8,8 +8,8 @@ import hangman_title
 
 title = hangman_title.hangman_title_graphix
 game_progress = hangman_status.hang_man_status
-word_list = []
 word_length = int(random.randint(5, 17))
+word_list = []
 display_dashes = ""
 word_vault = []
 guessed_vault = []
@@ -17,37 +17,67 @@ guessed_wrong_vault = []
 revealed_word = ""
 new_word = ""
 strikes = 0
-
+game_over = False
 quit = False
 
+def reset():
+    global word_list
+    word_list = []
+    global display_dashes
+    display_dashes = ""
+    global word_vault
+    word_vault = []
+    global guessed_vault
+    guessed_vault = []
+    global guessed_wrong_vault 
+    guessed_wrong_vault = []
+    global revealed_word 
+    revealed_word = ""
+    global new_word
+    new_word = ""
+    global strikes
+    strikes = 0
+    global game_over
+    game_over = False
+    global quit
+    quit = False
+    print(title)
 
-print(title)
+reset()
 
-def word_creation(arg1_display_dashes, arg2_new_word, arg3_word_list, arg4_revealed_word):
+def word_creation():
+    global new_word
+    global word_list
+    global display_dashes
+    global strikes
+    global revealed_word
+
     for i in range(1):
-        arg2 = word_create.word(word_min_length=word_length, word_max_length=word_length)
-        arg3_word_list = list(arg2_new_word)
-        arg4_revealed_word = arg2_new_word
-        print(f"==> arg3_word_list {arg3_word_list}")
-        print("Word "+str(i+1)+" : ", arg2_new_word)
+        new_word = word_create.word(word_min_length=word_length, word_max_length=word_length)
+        word_list = list(new_word)
+        revealed_word = new_word
+        #print(f"==> word_list {word_list}")
+        #print("Word "+str(i+1)+" : ", new_word)
 
     for i in range(word_length):
-        arg1_display_dashes += " _ "
+        display_dashes += " _ "
 
     for i in range(word_length):
         word_vault.append(" _ ")
     print(game_progress[strikes])
-    print(arg1_display_dashes)
+    print(display_dashes)
 
 # print(f"==>> word_vault: {word_vault}")
 
-word_creation(display_dashes, new_word, word_list, revealed_word)
+word_creation()
 
 
 
-def main_game(arg1):
-    game_over = False
-    strikes = 0
+def main_game():
+    global game_over
+    global strikes
+    global display_dashes
+    global game_over
 
     while not game_over:
     
@@ -88,15 +118,19 @@ def main_game(arg1):
                 game_over=True
                 print ("\n########################### YOU WON!!!!!!!!!! ###########################\n")
                 print(title)
+    restart_prompt = input("Would you like to try again? Yes(Y) or No (N)?: ").lower()
+
+    if restart_prompt=="y":
+        reset()
+        word_creation()
+        main_game()
+    else:
+        quit = True
+        print("Goodbye")
     #print(f"==>> word_vault[i]: {word_vault}")
 
-main_game(display_dashes)
-restart_prompt = input("Would you like to try again? Yes(Y) or No (N)?: ").lower()
-if restart_prompt=="y":
-    game_over = False
-    main_game(display_dashes)
-else:
-    quit = True
+main_game()
+
 
     #print(f"==>> display_dashes: {display_dashes}")
 

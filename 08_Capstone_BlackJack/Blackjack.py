26cacafe02d62d47ad91bb87acd5f8accbe1467e
Jessie Jones <Jessie.Jones14@Protonmail.com>
2025-01-20 23:42:46 -0500

    got hit or stand working error with tuple

--- 08_Capstone_BlackJack/Blackjack.py
+++ 08_Capstone_BlackJack/Blackjack.py
@@ -10,6 +10,7 @@ dealer_hand = {}
 players_hand = {}
 dealer_cards = ()
 player_cards = ()
+card_count = 2
 
 
 def shuffle():
@@ -35,11 +36,6 @@ def dealer_shuffle():
     for i in range(130):
         shuffle()
         i += 1
-
-dealer_shuffle()
-print(shuffled_deck)
-
-input("To Deal Hand press ENTER ... ")
 
 def deal():
     global deck
@@ -69,13 +65,60 @@ def deal():
     dealer_cards = dealer_hand[keys_dealer[0]]["face"] + dealer_hand[keys_dealer[0]]["symbol"], dealer_hand[keys_dealer[0]]["value"], dealer_hand[keys_dealer[1]]["face"] + dealer_hand[keys_dealer[1]]["symbol"], dealer_hand[keys_dealer[1]]["value"]
     player_cards =  players_hand[keys_player[0]]["face"] + players_hand[keys_player[0]]["symbol"] , players_hand[keys_player[0]]["value"] , players_hand[keys_player[1]]["face"]+ players_hand[keys_player[1]]["symbol"], players_hand[keys_player[1]]["value"]
     
-    print(f"DEALER HAND: ## and {dealer_cards[2]}. The DEALER has {dealer_cards[3]}PTS")
-    print(f"PLAYER HAND: {player_cards[0]} and {player_cards[2]}.  The PLAYER has {player_cards[1] + player_cards[3]}PTS")
-    return dealer_hand, players_hand
-print(deck)
-deal()
+    print(f"DEALER HAND: ##  and {dealer_cards[2]}. The DEALER has {dealer_cards[3]}PTS")
+    print(f"PLAYER HAND: {player_cards[0]}  and {player_cards[2]}. The PLAYER has {player_cards[1] + player_cards[3]}PTS")
+    return dealer_hand, players_hand, dealer_cards, player_cards
+
 
 def hit_stand():
-    global dealer_hand
+    global deck
     global players_hand
-    hit_or_stand = input("Would you like to HIT or STAND?")
+    global card_count
+    new_player_cards = []
+    hit_or_stand = input("Would you like to HIT (H) or STAND (S)?").lower()
+    if hit_or_stand == "s":
+        print(players_hand)
+        return
+    elif hit_or_stand == "h":
+        #Picking a the unknown but NEXT key from the Deck dictionary
+        card = next(iter(deck))
+        #Using that key to put the card from the top of the deck in the players hand
+        players_hand[card] = deck.pop(card)
+        #Getting all of the unknown key values from the dictionary to iterate through
+        keys_player = list(players_hand.keys())
+
+        #Debug Prints
+        #print(players_hand)
+        #print(keys_player)
+        #iterating through to create a new player card list (previous used a tuple by accident need to homogenize it)
+        
+        for i in range(card_count+1):
+            
+            new_player_cards += [players_hand[keys_player[i]]["face"] + players_hand[keys_player[i]]["symbol"]]
+            new_player_cards += [players_hand[keys_player[i]]["value"]]
+        card_print ="PLAYER HAND: " 
+        card_print2 = "==> SCORE: "
+        card_print3 = 0  
+        for i in range((card_count+1)*2):
+            if i % 2 == 0: 
+                card_print +=  new_player_cards[i] + " "
+            else:
+                card_print3 +=  new_player_cards[i]
+    #Wanted to create a recursion but I think it best to do that in a formal try except block        
+    else:
+        print("You must type H or S ...")
+        hit_or_stand()
+    #can't access this might have to make the above a list would be easier append that way
+    print(f"DEALER HAND: ##  and {dealer_cards[2]}. The DEALER has {dealer_cards[3]}PTS")
+    
+    print("PLAYER HAND :"+ card_print,card_print2,card_print3)
+
+try:
+    dealer_shuffle()
+    print(shuffled_deck)
+    input("To Deal Hand press ENTER ... ")
+    print(deck)
+    deal()
+    hit_stand()
+except ValueError as e:
+    print(f"Error: {e}")
